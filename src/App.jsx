d712446e5222e6a786ecf5743dec84f7a4bf8c86
Olagoke Olorunkosebi <olagokeolorunkosebi@gmail.com>
2024-06-12 14:12:01 +0100

    Learning in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
+import { useState, createContext, useContext } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <h1>Count: {count}</h1>
-        <button onClick={() => setCount(count + 1)}>Increase</button>
-        <button onClick={() => setCount(count - 1)}>Decrease</button>
-        <button onClick={() => setCount(0)}>Reset</button>
-      </div>
-    </>
+    <section className="main-container" style={divStyle}>
+      <UserProfile />
+    </section>
   );
 }
 
